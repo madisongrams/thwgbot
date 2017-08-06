218b2b29d7a5040199c0e2da6f3a17298d91a2b9
Madison Grams <mgrams3@gatech.edu>
2017-08-06 17:48:20 -0400

    First Commit

---
+++ thwgbot.py
@@ -0,0 +1,8 @@
+import tweepy
+import os
+
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
+api = tweepy.API(auth)
+
+#api.update_status("Hello, World! #THWg")
