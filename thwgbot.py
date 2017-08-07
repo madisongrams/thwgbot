49262254359db6ca870204aff4b06f43d21a4aee
Madison Grams <mgrams3@gatech.edu>
2017-08-06 20:45:51 -0400

    print statement added for test

--- thwgbot.py
+++ thwgbot.py
@@ -2,6 +2,7 @@ import tweepy
 import os
 import time
 
+print(os.environ['CONSUMER_KEY'])
 auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
 auth.set_access_token(os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
 api = tweepy.API(auth)
