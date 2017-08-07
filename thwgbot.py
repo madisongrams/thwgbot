982a9a76d63c3649931e82a5e269a19234e854e0
Madison Grams <mgrams3@gatech.edu>
2017-08-06 20:40:42 -0400

    modifying bot code

--- thwgbot.py
+++ thwgbot.py
@@ -8,34 +8,44 @@ api = tweepy.API(auth)
 
 #api.update_status("Hello, World! #THWg")
 lastUgaId = -1
+oldUgaId = -1
 lastGtId = -1
-while(True):
-    num = 0
-    for tweet in tweepy.Cursor(api.search, q="go dawgs OR godawgs", since_id = lastUgaId, lang="en").items():
-        if num == 0:
-            lastUgaId = tweet.id
-        elif num == 5:
+oldGtId = -1
+sleep = False
+while True:
+    first = True
+    for status in tweepy.Cursor(api.search, q="go dawgs OR godawgs", lang ="en").items(5):
+        if status.id == oldUgaId:
             break
+        if first:
+            oldUgaId = lastUgaId
+            lastUgaId = status.id
+            first = False
         try:
-            api.update_status(status="Go Jackets! #THWg", in_reply_to_status_id=tweet.id)
+            api.update_status(status="Go Jackets! #THWg", in_reply_to_status_id=status.id)
+            sleep(10)
+        except tweepy.RateLimitError:
+            sleep(900)
+        except tweepy.TweepError as e:
+            break #for now merely exit out of loop for now
+    first = True
+    for status in tweepy.Cursor(api.search, q="go jackets OR gojackets", lang="en").items(5):
+        if status.id == oldGtId:
+            break
+        if first:
+            oldGtId = lastGtId
+            lastGtId = status.id
+            first = False
+        try:
+            api.update_status(status="Go Jackets! #TogetherWeSwarm", in_reply_to_status_id=status.id)
             sleep(10)
         except tweepy.RateLimitError:
             sleep(900)
         except tweepy.TweepError as e:
             break #for now merely exit out of loop for now
         num += 1
-    num = 5
-    for tweet in tweepy.Cursor(api.search, q="go jackets OR gojackets", since_id = lastGtId, lang="en").items():
-        if num == 5:
-            lastGtId = tweet.id
-        if num == 10:
-            break
-        try:
-            api.update_status(status="Go Jackets! #TogetherWeSwarm", in_reply_to_status_id=tweet.id)
-            sleep(10)
-        except tweepy.RateLimitError:
-            sleep(900)
-        except tweepy.TweepError as e:
-            break #for now merely exit out of loop for now
-        num += 1
-    sleep(300) #sleep for 5 minutes
+    if (sleep):
+        sleep(300) #sleep for 5 minutes
+        sleep = False
+    else:
+        sleep = True
